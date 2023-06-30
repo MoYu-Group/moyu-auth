b1973111b21d597d16b3c03690a05dbea6fb6eb2
饭饭 <76698242@qq.com>
2023-07-01 00:41:17 +0800

    :sparkles: add moyu-auth-client-demo 模块，用于接入登录系统的演示

--- moyu-auth-start/src/main/java/io/github/moyugroup/auth/controller/HelloController.java
+++ moyu-auth-start/src/main/java/io/github/moyugroup/auth/controller/HelloController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by fanfan on 2022/05/12.
  */
 @RestController
-public class HelloWorldController {
+public class HelloController {
 
     @GetMapping("hello")
     public String helleWorld() {
-        return "hello world !!!";
+        return "hello Moyu-Auth-Server !!!";
     }
 }
